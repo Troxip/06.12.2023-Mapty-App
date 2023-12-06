bae3f73c2020bc4017c5af06806be7a46b7f77b8
Abdurahim <abdurahim.gaforov9547@gmail.com>
2023-12-06 22:16:46 +0100

    Some Updates

--- script.js
+++ script.js
@@ -11,12 +11,64 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+const clear = function () {
+  inputDistance.value = "";
+  inputCadence.value = "";
+  inputDuration.value = "";
+  inputElevation.value = "";
+};
+
+let map, mapEvent;
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      map = L.map("map").setView(coords, 13);
+
+      L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on("click", function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+      });
     },
     function () {
       console.log("Could not get your position");
     }
   );
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("Workout")
+    .openPopup();
+  form.classList.add("hidden");
+  clear();
+});
+
+inputType.addEventListener("change", function (e) {
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+});
